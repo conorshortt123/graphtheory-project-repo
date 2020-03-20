e27132312c6400fe6ef8f9b3d6fa3c2ffc06b268
CONOR SHORTT <G00360253@gmit.ie>
2020-03-20 19:26:07 +0000

    Seperated shunting.py into it's own script

--- regex.py
+++ regex.py
@@ -1,6 +1,8 @@
 # Conor Shortt
 # Classes used in Thompson's Construction.
 
+from shunting import shunt
+
 class State:
     """A state with one or two edges, all edges labelled by label."""
     # Constructor for the class.
@@ -16,48 +18,6 @@ class Fragment:
     def __init__(self, start, accept):
         self.start = start
         self.accept = accept
-
-def shunt(infix):
-    """Return the infix regular expression in postfix."""
-    infix = list(infix)[::-1]
-
-    # Operator stack.
-    opers, postfix = [], []
-
-    # Operator precedence.
-    prec = {'*': 100, '.': 80, '|': 60, ')': 40, '(': 20}
-
-    # Loop through the input one character at a time.
-    while infix:
-    # Pop a character from the input.
-        c = infix.pop()
-
-        # Decide what to do based on the character.
-        if c == '(':
-            # Push an open bracket to the opers stack.
-            opers.append(c)
-        elif c == ')':
-            # Pop the operators stack until you find an (.
-            while opers[-1] != '(':
-                postfix.append(opers.pop())
-            # Get rid of the '('.
-            opers.pop()
-        elif c in prec:
-            # Push any operators on the opers stack with higher prec to the output.
-            while opers and prec[c] < prec[opers[-1]]:
-                postfix.append(opers.pop())
-            # Push c to the operator stack.
-            opers.append(c)
-        else:
-        # Typically, we just push the character to the output.
-            postfix.append(c)
-    
-    # Pop all the operators to the output.
-    while opers:
-        postfix.append(opers.pop())
-
-    # Convert output list to string.
-    return ''.join(postfix)
 
 def compile(infix):
     """Return an NFA fragment representing the infix regular expression"""
@@ -159,7 +119,6 @@ def match(regex, s):
     return nfa.accept in current
 
 if __name__ == "__main__":
-
     tests = [
         ["a.b|b*", "bbbbb", True],
         ["a.b|b*", "bbx", False],
--- shunting.py
+++ shunting.py
@@ -1,55 +1,44 @@
 # Conor Shortt
 # The Shunting Yard algorithm for regular expressions.
 
-# The input.
-infix = "(a|b).c*"
-print("Input is: ", infix)
-# Expect output: "ab|c*."
-print("Expected: ", "ab|c*.")
+def shunt(infix):
+	"""Return the infix regular expression in postfix."""
+	infix = list(infix)[::-1]
 
-# Convert to stack list
-infix = list(infix)[::-1]
+	# Operator stack.
+	opers, postfix = [], []
 
-# Operator stack.
-opers = []
+	# Operator precedence.
+	prec = {'*': 100, '.': 80, '|': 60, ')': 40, '(': 20}
 
-# Output list.
-postfix = []
+	# Loop through the input one character at a time.
+	while infix:
+		# Pop a character from the input.
+		c = infix.pop()
 
-# Operator precedence.
-prec = {'*': 100, '.': 80, '|': 60, ')': 40, '(': 20}
+		# Decide what to do based on the character.
+		if c == '(':
+			# Push an open bracket to the opers stack.
+			opers.append(c)
+		elif c == ')':
+			# Pop the operators stack until you find an (.
+			while opers[-1] != '(':
+				postfix.append(opers.pop())
+			# Get rid of the '('.
+			opers.pop()
+		elif c in prec:
+			# Push any operators on the opers stack with higher prec to the output.
+			while opers and prec[c] < prec[opers[-1]]:
+				postfix.append(opers.pop())
+			# Push c to the operator stack.
+			opers.append(c)
+		else:
+			# Typically, we just push the character to the output.
+			postfix.append(c)
 
-# Loop through the input one character at a time.
-while infix:
-    # Pop a character from the input.
-    c = infix.pop()
+	# Pop all the operators to the output.
+	while opers:
+		postfix.append(opers.pop())
 
-    # Decide what to do based on the character.
-    if c == '(':
-        # Push an open bracket to the opers stack.
-        opers.append(c)
-    elif c == ')':
-        # Pop the operators stack until you find an (.
-        while opers[-1] != '(':
-            postfix.append(opers.pop())
-        # Get rid of the '('.
-        opers.pop()
-    elif c in prec:
-        # Push any operators on the opers stack with higher prec to the output.
-        while opers and prec[c] < prec[opers[-1]]:
-            postfix.append(opers.push())
-        # Push c to the operator stack.
-        opers.append(c)
-    else:
-        # Typically, we just push the character to the output.
-        postfix.append(c)
-
-# Pop all the operators to the output.
-while opers:
-    postfix.append(opers.pop())
-
-# Convert output list to string.
-postfix = ''.join(postfix)
-
-# Print the result.
-print("Output is:", postfix)
+	# Convert output list to string.
+	return ''.join(postfix)
